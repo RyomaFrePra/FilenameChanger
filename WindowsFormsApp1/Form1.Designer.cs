7a5d0dcb8e448496ff9f3930c9fa0d229dae0886
ЯoomeR <ryomasenbra@gmail.com>
2023-10-12 17:14:52 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.select_file_button = new System.Windows.Forms.Button();
+            this.textBoxCompany = new System.Windows.Forms.TextBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // select_file_button
+            // 
+            this.select_file_button.Location = new System.Drawing.Point(340, 363);
+            this.select_file_button.Name = "select_file_button";
+            this.select_file_button.Size = new System.Drawing.Size(108, 23);
+            this.select_file_button.TabIndex = 0;
+            this.select_file_button.Text = "ファイルを選択";
+            this.select_file_button.UseVisualStyleBackColor = true;
+            this.select_file_button.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxCompany
+            // 
+            this.textBoxCompany.Location = new System.Drawing.Point(507, 141);
+            this.textBoxCompany.Name = "textBoxCompany";
+            this.textBoxCompany.Size = new System.Drawing.Size(100, 19);
+            this.textBoxCompany.TabIndex = 1;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(507, 235);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 19);
+            this.textBoxAmount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(440, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "企業名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(454, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "金額";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(158, 126);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(90, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "日付";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(622, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "円";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(32, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(575, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "使い方：日付・企業名・金額を入力し、「ファイルを選択」すると、ファイル名が変更されます。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.textBoxCompany);
+            this.Controls.Add(this.select_file_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button select_file_button;
+        private System.Windows.Forms.TextBox textBoxCompany;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // OpenFileDialogを設定
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "ファイルを選択";
+            openFileDialog.Filter = "すべてのファイル|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // ユーザーがファイルを選択した場合
+                string selectedFilePath = openFileDialog.FileName;
+
+                // 新しいファイル名をテキストボックスから取得
+                string newFileNameCompany = textBoxCompany.Text;
+                string newFileNameAmount = textBoxAmount.Text;
+
+                if (!string.IsNullOrEmpty(newFileNameCompany) && !string.IsNullOrEmpty(newFileNameAmount))
+                {
+                    try
+                    {
+                        // オリジナルのファイル名から拡張子を抽出
+                        string originalExtension = Path.GetExtension(selectedFilePath);
+
+                        // 現在の日付を取得
+                        DateTime currentDate = monthCalendar1.SelectionStart;
+                        string datePart = currentDate.ToString("yyyy_MM_dd");
+
+                        // 新しいファイル名を生成（日付を含む）
+                        string newFileName = datePart + "_" + newFileNameCompany + "_" + newFileNameAmount + originalExtension;
+
+                        // ファイル名を変更
+                        string newFilePath = Path.Combine(Path.GetDirectoryName(selectedFilePath), newFileName);
+                        File.Move(selectedFilePath, newFilePath);
+
+                        // 成功メッセージを表示
+                        MessageBox.Show("ファイル名が変更されました。", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("ファイル名の変更に失敗しました。\nエラーメッセージ: " + ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("新しいファイル名、会社名、金額を入力してください。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
